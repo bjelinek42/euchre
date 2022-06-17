91a3355454f2831bd6a2f021ff8e2eb416acba47
ben <bjelinek42@gmail.com>
2022-06-17 10:33:47 -0600

    chose random suit, put card in hand, remove from deck

---
+++ euchre.rb
@@ -0,0 +1,29 @@
+class Euchre
+
+  def initialize
+    @deck = {"spades" => ["Ace", "King", "Queen", "Jack", "10", "9"], "clubs" => ["Ace", "King", "Queen", "Jack", "10", "9"], "hearts" => ["Ace", "King", "Queen", "Jack", "10", "9"], "diamonds" => ["Ace", "King", "Queen", "Jack", "10", "9"]}
+    @human = {"spades" => [], "clubs" => [], "hearts" => [], "diamonds" => []}
+    @computer1 = {"spades" => [], "clubs" => [], "hearts" => [], "diamonds" => []}
+    @computer2 = {"spades" => [], "clubs" => [], "hearts" => [], "diamonds" => []}
+    @computer3 = {"spades" => [], "clubs" => [], "hearts" => [], "diamonds" => []}
+    @burn_pile = {"spades" => [], "clubs" => [], "hearts" => [], "diamonds" => []}
+  end
+
+  def deal
+    suits = ["spades", "clubs", "hearts", "diamonds"]
+    4.times do
+      suit = suits.sample
+      value = @deck[suit].sample
+      @human[suit] << value #puts card in hand
+      @deck[suit].delete(value) # removes from deck
+      p @human
+      p @deck
+
+    end
+  
+  end
+
+end
+
+euchre = Euchre.new
+euchre.deal

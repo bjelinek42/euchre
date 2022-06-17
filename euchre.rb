580c10d82d70c7d38c74a46ec6a45ff51019ac52
ben <bjelinek42@gmail.com>
2022-06-17 11:06:30 -0600

    flip trump card

--- euchre.rb
+++ euchre.rb
@@ -6,12 +6,29 @@ class Euchre
     @computer1 = {"spades" => [], "clubs" => [], "hearts" => [], "diamonds" => []}
     @computer2 = {"spades" => [], "clubs" => [], "hearts" => [], "diamonds" => []}
     @computer3 = {"spades" => [], "clubs" => [], "hearts" => [], "diamonds" => []}
-    @burn_pile = {"spades" => [], "clubs" => [], "hearts" => [], "diamonds" => []}
+    @flip_card = {}
   end
 
-  def deal
+  def full_deal
+    @human = player_deal
+    @computer1 = player_deal
+    @computer2 = player_deal
+    @computer3 = player_deal
+    trump
+    p @human
+    p @computer1
+    p @computer2
+    p @computer3
+    p @flip_card
+
+    
+  
+  end
+
+  def player_deal
+    hand = {"spades" => [], "clubs" => [], "hearts" => [], "diamonds" => []}
     suits = ["spades", "clubs", "hearts", "diamonds"]
-    24.times do
+    5.times do
       while true #prevents taking from suits with no cards left
         suit = suits.sample
         if @deck[suit] != []
@@ -19,16 +36,25 @@ class Euchre
         end
       end
       value = @deck[suit].sample
-      @human[suit] << value #puts card in hand
+      hand[suit] << value #puts card in hand
       @deck[suit].delete(value) # removes from deck
-      p @human
-      p @deck
-
     end
-  
+    return hand
+  end
+
+  def trump
+    suits = ["spades", "clubs", "hearts", "diamonds"]
+    while true #prevents taking from suits with no cards left
+      suit = suits.sample
+      if @deck[suit] != []
+        break
+      end
+    end
+    value = @deck[suit].sample
+    @flip_card[suit] = value
   end
 
 end
 
 euchre = Euchre.new
-euchre.deal
+euchre.full_deal
